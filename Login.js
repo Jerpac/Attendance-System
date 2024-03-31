b62853e892f40b5566c5b5cbb04adab90aacf554
Jeremy Pacheco <68563577+Jerpac@users.noreply.github.com>
2024-03-31 17:22:50 -0500

    Updated Login page to use UTD ID and Class Code

--- Login.js
+++ Login.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-    valid_users = {
-        "users": [
-            { "username": "tst100000", "password": "testUser10" },
-            { "username": "utd100000", "password": "utdUser5" },
-            { "username": "hey100000", "password": "Hello2" }
-        ]
-    }
+
+    console.log('Hello World')
+    user_from_db =
+        { "username": "1234567890", "password": "WHOOSH" }
+    var keys = Object.keys(user_from_db);
+    var values = Object.values(user_from_db);
+
+
     $("#login_button").click(function () {
-        //var url = "Attendance.html";
+        var url = "Attendance.html";
         //$(location).attr('href', url);
         var username = $("#user_input").val();
         var password = $('#pass_input').val();
-        console.log(username);
-        console.log(password);
+
+        if (username == values[0] && password == values[1]) {
+            $(location).attr('href', url);
+        } else {
+            alert('Wrong User Credentials!');
+        }
 
         //if () {
         //console.log('yah')
@@ -21,20 +26,20 @@ $(document).ready(function () {
         //}
 
         //start of AJAX demo
-
-        univ_fact_url = 'http://universities.hipolabs.com/search?country=United+States'
-        $.ajax({
-            url: univ_fact_url,
-            type: 'GET',
-            data: [],
-            success: function (data) {
-                console.log(data[2])
-            },
-            error: function (result) {
-                console.log('not worked');
-            }
-        });
-
+        /*
+       univ_fact_url = 'http://universities.hipolabs.com/search?country=United+States'
+       $.ajax({
+           url: univ_fact_url,
+           type: 'GET',
+           data: [],
+           success: function (data) {
+               console.log(data[0])
+           },
+           error: function (result) {
+               console.log('not worked');
+           }
+       });
+       */
 
         event.preventDefault();
     });
